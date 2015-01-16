a74d10947c54f5ea4b3bec925ae994a09e53687f
Orangestar <orangestar@cats4gold.net>
2015-01-15 22:27:18 -0600

    Catch exceptions and provide users with options.

--- compiler.py
+++ compiler.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
+import traceback
 
 def ignorePHP():
     global newCode
@@ -102,12 +103,33 @@ rawFileData.close()
 currentCharToCheck = 0
 newCode = ''
 print('GSLAUUA is now evaluating your code.')
-while len(code) != currentCharToCheck:
-    if code[currentCharToCheck] == '<':
-        startReplacing()
-    else:
-        newCode += code[currentCharToCheck] #There's probably a much better way to do this than write our code letter-for-letter.
-    currentCharToCheck += 1
+try:
+    while len(code) != currentCharToCheck:
+        if code[currentCharToCheck] == '<':
+            startReplacing()
+        else:
+            newCode += code[currentCharToCheck] #There's probably a much better way to do this than write our code letter-for-letter.
+        currentCharToCheck += 1
+except (RuntimeError, TypeError, NameError, IndexError) as e:
+    print('\n\
+        ========================================\n\
+        Woah! GSLAUUA just encountered an error!\n\
+        ========================================\n\
+The error Python gave us was "' + e.__class__.__name__ + '".\n\
+We recommend combing through your code and making sure that nothing is wrong\n\
+syntatically. Some things to watch out for:\n\
+ - You left a left carat (<) open. (I.E., with no right carat [>].)\n\
+ - You opened an attribute using a quote or apostrophe, but forgot to close it.\n\
+ - You opened an attribute with one opener, but closed it with a different one.\n\
+ - You opened up PHP or ASP, but didn\'t close it appropriately.\n\n\
+If all of these things have been checked and you\'re still getting an error, let\n\
+us know over at http://github.com/arctic5/SLAcompiler/issues\n\
+When you press enter, we\'ll give you some details about the error. Include\n\
+these details if you plan to post an issue to GitHub!')
+    input('Press enter to continue...')
+    print("\n" + traceback.format_exc())
+    input('Press enter to continue...')
+    sys.exit()
 
 #print(newCode)
 #input('Press Enter')
